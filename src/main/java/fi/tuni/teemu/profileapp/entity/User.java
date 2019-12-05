37c09d70159c13dda95481343641a72359266ddc
Teemu <teemu.salminen96@gmail.com>
2019-12-06 00:16:48 +0200

    Implement User entity

--- src/main/java/fi/tuni/teemu/profileapp/entity/User.java
+++ src/main/java/fi/tuni/teemu/profileapp/entity/User.java
@@ -1,5 +1,45 @@
 package fi.tuni.teemu.profileapp.entity;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class User {
+	
+	@Id
+	private String email;
+	
+	@Column(name = "username")
+	private String username;
+	
+	@Column(name = "description")
+	private String description;
+	
+	public User() {}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 }
