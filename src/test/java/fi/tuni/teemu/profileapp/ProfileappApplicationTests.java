0d1631b393577e63e1bc0ad37caad6f16855a898
Teemu <teemu.salminen96@gmail.com>
2019-12-05 22:55:43 +0200

    Initial commit

---
+++ src/test/java/fi/tuni/teemu/profileapp/ProfileappApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.tuni.teemu.profileapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
