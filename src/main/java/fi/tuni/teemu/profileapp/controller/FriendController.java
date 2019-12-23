266502fff374db2f62afc2d911fff0a335df90db
Teemu <teemu.salminen96@gmail.com>
2019-12-23 04:19:12 +0200

    Add class FriendController

---
+++ src/main/java/fi/tuni/teemu/profileapp/controller/FriendController.java
@@ -0,0 +1,14 @@
+package fi.tuni.teemu.profileapp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import fi.tuni.teemu.profileapp.entity.Friend;
+
+@RestController
+public class FriendController {
+	
+	public Friend addFriend() {
+		return null;
+	}
+
+}
