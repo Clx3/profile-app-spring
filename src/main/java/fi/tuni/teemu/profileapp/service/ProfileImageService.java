3dcd1d45747b395f905b531cfc4f7c13b32c255b
Teemu <teemu.salminen96@gmail.com>
2020-01-07 23:51:51 +0200

    File uploading modifications

--- src/main/java/fi/tuni/teemu/profileapp/service/ProfileImageService.java
+++ src/main/java/fi/tuni/teemu/profileapp/service/ProfileImageService.java
@@ -2,14 +2,10 @@ package fi.tuni.teemu.profileapp.service;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,6 +15,7 @@ import fi.tuni.teemu.profileapp.entity.Profile;
 @Service
 public class ProfileImageService {
 	
+	// Seems to be working on Heroku..
     public static final String UPLOAD_DIR = "src/main/resources/images/";
     
     @Autowired
@@ -27,7 +24,6 @@ public class ProfileImageService {
 	public void storeFile(MultipartFile file, HttpServletRequest request) throws IOException {
 		Profile userProfile = profileService.findMe();
 		String fileName = String.format("Profile_%d.png", userProfile.getId());
-		System.out.println(Paths.get(UPLOAD_DIR).toAbsolutePath());
 
 		String path = UPLOAD_DIR + fileName;
 		FileCopyUtils.copy(file.getBytes(), new File(path));
