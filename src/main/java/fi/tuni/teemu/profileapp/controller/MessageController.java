8f3a435d13b0d432d485d0a9357c4fd0fd4549fd
Teemu <teemu.salminen96@gmail.com>
2020-01-09 23:00:34 +0200

    Added PUT method to send a message

--- src/main/java/fi/tuni/teemu/profileapp/controller/MessageController.java
+++ src/main/java/fi/tuni/teemu/profileapp/controller/MessageController.java
@@ -1,14 +1,22 @@
 package fi.tuni.teemu.profileapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import fi.tuni.teemu.profileapp.entity.Message;
 import fi.tuni.teemu.profileapp.service.MessageService;
 
 @RestController
 public class MessageController {
 	
 	@Autowired
-	private MessageService service;
+	private MessageService messageService;
+	
+	@PutMapping(value = "message/")
+	public Message putMessage(@RequestBody Message message) {
+		return messageService.save(message);
+	}
 
 }
--- src/main/java/fi/tuni/teemu/profileapp/service/MessageService.java
+++ src/main/java/fi/tuni/teemu/profileapp/service/MessageService.java
@@ -3,6 +3,8 @@ package fi.tuni.teemu.profileapp.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fi.tuni.teemu.profileapp.entity.Message;
+import fi.tuni.teemu.profileapp.entity.Profile;
 import fi.tuni.teemu.profileapp.repository.MessageRepository;
 
 @Service
@@ -10,5 +12,16 @@ public class MessageService {
 	
 	@Autowired
 	private MessageRepository messageRepository;
+	
+	@Autowired
+	private ProfileService profileService;
+	
+	public Message save(Message message) {
+		Profile userProfile = profileService.findMe();
+		
+		message.setSenderId(userProfile.getId());
+		
+		return messageRepository.save(message);
+	}
 
 }
